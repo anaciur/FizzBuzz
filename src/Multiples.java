dbf524fc63dfd58cb07bd69ca20b10527fa1b5d3
Ana Ciur <ana.ciur@mail.utoronto.ca>
2023-09-14 14:52:29 -0400

    I created the multiples code

--- src/Multiples.java
+++ src/Multiples.java
@@ -4,10 +4,11 @@ public class Multiples {
         for (int i = 1; i < 1000; i ++){
             boolean multof3 = i % 3 == 0;
             boolean multof5 = i % 5 == 0;
+            // check if the number i is divisible by either 3 or 5
             if (multof3 || multof5) {
                 count++;
             }
         }
-        System.out.println(count);
+        System.out.println("The count of positive numbers divisible by either 3 or 5 below 1000 is: " + count);
     }
 }
